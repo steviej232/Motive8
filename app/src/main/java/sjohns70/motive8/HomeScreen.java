82d1ab92c9d215db56cc89eedb89e9e8907094fd
steviej232 <steviej232@gmail.com>
2016-09-14 16:02:17 -0400

    Optimized imports

--- app/src/main/java/sjohns70/motive8/HomeScreen.java
+++ app/src/main/java/sjohns70/motive8/HomeScreen.java
@@ -1,24 +1,20 @@
 package sjohns70.motive8;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.facebook.*;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class HomeScreen extends AppCompatActivity {
     public LoginButton loginButton;
