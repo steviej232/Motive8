76bc96cb021a19a1be529d4f8a43dbe9639ed5b5
zachho123 <zachho123@gmail.com>
2016-10-04 13:01:05 -0700

    Added method checkFirstRun() in HomeScreen Activity. Method checks on startup whether this is the first app run after installation, first run after updating, or a normal run.

--- app/src/main/java/sjohns70/motive8/HomeScreen.java
+++ app/src/main/java/sjohns70/motive8/HomeScreen.java
@@ -1,7 +1,10 @@
 package sjohns70.motive8;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +32,10 @@ public class HomeScreen extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.home_screen);
         initFacebook();
-        settings = (Button)findViewById(R.id.settings);
+
+        checkFirstRun();
+
+        settings = (Button) findViewById(R.id.settings);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,7 +43,8 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-        c_list = (Button)findViewById(R.id.company_list);
+
+        c_list = (Button) findViewById(R.id.company_list);
         c_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,7 +52,8 @@ public class HomeScreen extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-        circle_fill = (Button)findViewById(R.id.circle_fill);
+
+        circle_fill = (Button) findViewById(R.id.circle_fill);
         circle_fill.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,13 +62,13 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
-        if(isLoggedIn()){
+        if (isLoggedIn()) {
             Intent myIntent = new Intent(HomeScreen.this, MapsActivity.class);
             startActivity(myIntent);
         }
     }
 
-    public void initFacebook(){
+    public void initFacebook() {
         AppEventsLogger.activateApp(this);
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
@@ -70,17 +78,20 @@ public class HomeScreen extends AppCompatActivity {
                 Intent myIntent = new Intent(HomeScreen.this, MapsActivity.class);
                 startActivity(myIntent);
             }
+
             @Override
             public void onCancel() {
-                Toast.makeText(getApplicationContext(),"Canceled logging in",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Canceled logging in", Toast.LENGTH_SHORT).show();
             }
+
             @Override
             public void onError(FacebookException exception) {
-                Toast.makeText(getApplicationContext(),"Error logging in",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Error logging in", Toast.LENGTH_SHORT).show();
             }
         });
         loginButton.setReadPermissions("user_friends");
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -92,4 +103,43 @@ public class HomeScreen extends AppCompatActivity {
         return accessToken != null;
     }
 
+    /**
+     * This method uses shared preferences to save the current app version code. It will check if
+     * this startup of the app is the first launch after installation, first launch after updating,
+     * or just a normal run.
+     */
+    private void checkFirstRun() {
+        final String PREFS_NAME = "Motive8PrefsFile";
+        final String PREF_VERSION_CODE_KEY = "version_code";
+        final int DOESNT_EXIST = -1;
+
+        // Get current version code
+        int currentVersionCode = BuildConfig.VERSION_CODE;
+
+        // Get saved version code from shared preferences
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST);
+
+        // Compare current and saved version codes to determine run type
+        if (currentVersionCode == savedVersionCode) {
+            // Normal run
+            Toast.makeText(getApplicationContext(), "Normal run", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (savedVersionCode == DOESNT_EXIST) {
+            // First run
+            Toast.makeText(getApplicationContext(), "First run", Toast.LENGTH_SHORT).show();
+        }
+        else if (currentVersionCode > savedVersionCode) {
+            // This is an upgrade
+            Toast.makeText(getApplicationContext(), "Upgraded", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // The user somehow reverted versions of the app
+            return;
+        }
+
+        // Update shared preferences with current version code
+        prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).apply();
+    }
 }
