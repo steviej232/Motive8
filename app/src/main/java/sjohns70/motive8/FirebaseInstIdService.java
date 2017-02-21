c3149529348f1ecb283baf6f50b552ca8be71d68
steviej232 <steviej232@gmail.com>
2017-02-21 14:50:14 -0800

    2-21 update

--- app/src/main/java/sjohns70/motive8/FirebaseInstIdService.java
+++ app/src/main/java/sjohns70/motive8/FirebaseInstIdService.java
@@ -4,8 +4,6 @@ package sjohns70.motive8;
  * Created by Steven on 2/15/2017.
  */
 
-import android.widget.Toast;
-
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -18,7 +16,7 @@ public class FirebaseInstIdService extends FirebaseInstanceIdService{
         //Get hold of the registration token
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         //Log the token
-        Toast.makeText(getApplicationContext(),"refreshed:"+refreshedToken,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(),"refreshed:"+refreshedToken,Toast.LENGTH_SHORT).show();
     }
     private void sendRegistrationToServer(String token) {
         //Implement this method if you want to store the token on your server
