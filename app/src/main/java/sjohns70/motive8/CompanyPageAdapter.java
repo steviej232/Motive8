72210d22bdaed713e5ff4b3de6e0269d94a8fb0d
kendallgassner <kendall.gassner@yahoo.com>
2017-02-28 12:04:31 -0800

    the decscription in viewpager was wrong

--- app/src/main/java/sjohns70/motive8/CompanyPageAdapter.java
+++ app/src/main/java/sjohns70/motive8/CompanyPageAdapter.java
@@ -32,12 +32,13 @@ public class CompanyPageAdapter extends PagerAdapter  {
 
         ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.my_pager_list, collection, false);
         TextView name = (TextView) layout.findViewById(R.id.pager_business_name);
-        TextView description = (TextView) layout.findViewById(R.id.pager_business_name);
+        TextView description = (TextView) layout.findViewById(R.id.pager_description);
         if(bussineses.size() > position) {
             BusinessData business = bussineses.get(position);
             name.setText(business.getCompany_name());
             description.setText(business.getDescription());
             setFonts(name);
+            setFonts(description);
         }
         collection.addView(layout);
         return layout;
