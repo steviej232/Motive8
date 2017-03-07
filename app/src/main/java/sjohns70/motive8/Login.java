071411c9f6c706b2e1497285c6ecc3e641f2fd66
zachho123 <zachho123@gmail.com>
2017-03-07 11:43:07 -0800

    Implemented forgot password functionality on login screen. Users can now input their email address and receive an email that will allow them to reset their password.

--- app/src/main/java/sjohns70/motive8/Login.java
+++ app/src/main/java/sjohns70/motive8/Login.java
@@ -4,11 +4,14 @@ package sjohns70.motive8;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +31,7 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
@@ -58,6 +62,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     private Button login_submit;
     private TextView login_sign_up;
     private TextView login_sign_up_text;
+    private TextView login_forgot_password;
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -69,6 +74,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     private int RC_SIGN_IN = 1;
     public GoogleApiClient mGoogleApiClient;
     private SharedPreferences pref;
+    private String emailAddress = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +85,8 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         login_email = (EditText) findViewById(R.id.login_email);
         login_password = (EditText) findViewById(R.id.login_password);
         login_sign_up = (TextView) findViewById(R.id.sign_up);
-        login_sign_up_text =(TextView) findViewById(R.id.sign_up_text);
+        login_sign_up_text = (TextView) findViewById(R.id.sign_up_text);
+        login_forgot_password = (TextView) findViewById(R.id.forgot_password);
         login_submit = (Button) findViewById(R.id.submit_login);
 
 //        String UID = "";
@@ -105,6 +112,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 }
             }
         };
+
         initFacebook();
 
         login_submit.setOnClickListener(new View.OnClickListener() {
@@ -114,11 +122,41 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
             }
         });
+
         login_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signup = new Intent(Login.this, SignUp.class);
                 startActivity(signup);
+            }
+        });
+
+        login_forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create alert dialog to accept user's email address
+                AlertDialog.Builder passResetBuilder = new AlertDialog.Builder(Login.this);
+                passResetBuilder.setTitle("Enter Email Address:");
+                final EditText input = new EditText(Login.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                passResetBuilder.setView(input);
+
+                passResetBuilder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        emailAddress = input.getText().toString();
+                        sendResetEmail(emailAddress);
+                    }
+                });
+                passResetBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                passResetBuilder.show();
             }
         });
 
@@ -168,6 +206,21 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 }
             }
         });
+    }
+
+    private void sendResetEmail(String emailAddress) {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent");
+                            Toast.makeText(Login.this, "Password Reset Email Sent", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 
@@ -280,6 +333,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         login_email.setTypeface(bebasFont);
         login_password.setTypeface(bebasFont);
         login_submit.setTypeface(bebasFont_bold);
+        login_forgot_password.setTypeface(bebasFont_bold);
         title.setTextSize(100);
     }
 
